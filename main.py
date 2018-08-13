0ded627a865e23a9be9990ef7e318d916cf63cac
Mohammed Khalil Ait Brahim <mohammed.khalil.ab@gmail.com>
2018-08-13 12:32:40 +0100

    Storage of problem sample tests and running binary features added.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import os
+import subprocess
+
 from typing import List, Tuple
 
 import requests
 from bs4 import BeautifulSoup
+
 
 def fetch(url: str) -> str:
     """Get the content of a webpage."""
     res = requests.get(url)
     if res.status_code == 200:
         return res.content
-    else:
-        raise Exception('Couldn\'t reach {}'.format(url))
+    raise Exception('Couldn\'t reach {}'.format(url))
+
 
 def parse(raw: str) -> List[Tuple[str, str]]:
     """Convert raw webpage of a problem content into HTML Tree and parse for tests.
@@ -17,7 +21,7 @@ def parse(raw: str) -> List[Tuple[str, str]]:
     Args:
         raw: A string representing the web page.
     Returns:
-        tests: A list of I/O of the tests to be used for a given problem page.
+        A list of I/O of the tests to be used for a given problem page.
     """
     soup = BeautifulSoup(raw, 'html.parser')
     tests = soup.find(attrs={
@@ -31,10 +35,58 @@ def parse(raw: str) -> List[Tuple[str, str]]:
     outputs = list(map(lambda o: o.pre.get_text('\n'), tests.find_all(attrs={
         'class': 'output'
     })))
-    return zip(inputs, outputs)
+    return list(zip(inputs, outputs))
+
+
+def store(contest: str, problem: str, io: List[Tuple[str, str]]):
+    """Store the input and output of a contest problem on disk.
+    The files are organized as numerical increments each in their own contest/problem directory.
+    This allows us to keep them separated and track error in which test cases.
+
+    Args:
+        contest: A unique contest identifier
+        problem: A problem in a contest
+        io: A list of input output tuples for the problem at hand
+    """
+    directory = '{}/.cf-samples/{}/{}'.format(
+        os.path.expanduser('~'), contest, problem)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for i, (inp, out) in enumerate(io):
+        with open('{}/{}.in'.format(directory, i), 'w') as f:
+            f.write(inp)
+        with open('{}/{}.out'.format(directory, i), 'w') as f:
+            f.write(out)
+
+
+def content(path: str) -> bytes:
+    """Returns bytes content of a file."""
+    with open(path, 'rb') as f:
+        return f.read()
+
+def test(contest: str, problem: str, binary: str):
+    """Test a binary against a const problem's IO
+
+    The test spawns a subprocess and runs the binary giving it the input
+    to the problem.
+
+    Args:
+        binary: An absolute path to the binary to test
+    """
+    path = '{}/.cf-samples/{}/{}'.format(
+        os.path.expanduser('~'), contest, problem)
+    directory = os.fsencode(path)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if not filename.endswith('in'):
+            continue
+        print(filename)
+        result = subprocess.run(binary, input=content(
+            '{}/{}'.format(path, filename)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(result.stdout.decode('utf-8'))
 
 
 if __name__ == '__main__':
-    for i, o in parse(fetch('https://codeforces.com/contest/1006/problem/A')):
-        print(i, o)
-        
+    # EXPERIMENTAL: Sample usage
+    store('1006', 'A', parse(fetch('https://codeforces.com/contest/1006/problem/A')))
+    test('1006', 'A', '{}/{}'.format(os.path.expanduser('~'), 'dev/lab/a.out'))
